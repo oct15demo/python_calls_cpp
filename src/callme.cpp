96c007a4167f17a7d56da8f7992c31db68e7d0e4
shoop <charles@Charless-MacBook-Air-3.local>
2022-03-13 14:46:00 -0400

    fix some indent issues from mixing tabs and spaces in .cpp files

--- src/callme.cpp
+++ src/callme.cpp
@@ -2,6 +2,6 @@
 #include <iostream>
 
 int main(int argc, char** argv){
-	printf("hello world\n");
-	std::cout<<__FUNCTION__<<" your logging message here "<<__FILE__<<":"<<__LINE__<<std::endl;
+   printf("hello world\n");
+   std::cout<<__FUNCTION__<<" your logging message here "<<__FILE__<<":"<<__LINE__<<std::endl;
 }
--- src/callmewithargs.cpp
+++ src/callmewithargs.cpp
@@ -2,10 +2,10 @@
 #include <iostream>
 
 int main(int argc, char** argv){
-	printf("hello python with args\n");
+   printf("hello python with args\n");
    printf("argc: %d\n", argc);
-	for(int i=0;i<argc;i++){
-		printf("argv[%d]: %s\n",i,argv[i]);
-	}
-	std::cout<<__FUNCTION__<<" your logging message here "<<__FILE__<<":"<<__LINE__<<std::endl;
+   for(int i=0;i<argc;i++){
+      printf("argv[%d]: %s\n",i,argv[i]);
+   }
+   std::cout<<__FUNCTION__<<" your logging message here "<<__FILE__<<":"<<__LINE__<<std::endl;
 }
